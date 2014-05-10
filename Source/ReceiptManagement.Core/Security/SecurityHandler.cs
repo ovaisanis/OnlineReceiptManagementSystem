5c805ca5bdfa4c87f83a654c42f53cf2f8433336
m-faisalnasir <mfaisal.nasir@hotmail.com>
2014-05-11 00:27:25 +0500

    added security handler file

---
+++ Source/ReceiptManagement.Core/Security/SecurityHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Collections = System.Collections.Generic;
+using Entities = ReceiptManagement.Common.Entities;
+using Helpers = ReceiptManagement.Common.Helpers;
+using System.Collections.Generic;
+
+namespace ReceiptManagement.Core.Security
+{
+    /// <summary>
+    /// SecurityHandler used to login, logout and authorize user into Core API
+    /// </summary>
+    public class SecurityHandler
+    {
+        #region Authorization
+
+      
+        public static void SetApiContext(Helpers.ApiContext apiContext)
+        {
+            if (!apiContext.IsContextInitialized)
+                apiContext.CurrentContext = new Model.OrmsContext(apiContext.ConnectionString);
+        }
+
+        #endregion
+    }
+}
