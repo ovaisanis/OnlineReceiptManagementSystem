7019d2dc65cff811c6a98277c2fe51c8c147ef75
Wahaj Ahmed Ansari <wahajahmedansari@gmail.com>
2014-05-11 02:17:52 +0500

    adding get method in login controller

--- Source/ReceiptManagement.RESTfulAPI/Controllers/LoginController.cs
+++ Source/ReceiptManagement.RESTfulAPI/Controllers/LoginController.cs
@@ -9,5 +9,23 @@ namespace ReceiptManagement.RESTfulAPI.Controllers
 {
     public class LoginController : ApiController
     {
+        public void Post(string username)
+        {
+
+        }
+
+        public string Get(string username, string password)
+        {
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+                return Boolean.TrueString.ToLower();
+            else
+                return Boolean.FalseString.ToLower();
+        }
+
+        // GET api/values/5
+        public string Get(string username)
+        {
+            return "this is your value="+username;
+        }
     }
 }
