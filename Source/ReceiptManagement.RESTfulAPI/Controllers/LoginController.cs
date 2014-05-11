59b0412129766dc020ebeb0935165e4b52647daf
Wahaj Ahmed Ansari <wahajahmedansari@gmail.com>
2014-05-11 23:19:54 +0500

    added post login functionality

--- Source/ReceiptManagement.RESTfulAPI/Controllers/LoginController.cs
+++ Source/ReceiptManagement.RESTfulAPI/Controllers/LoginController.cs
@@ -4,28 +4,26 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Models = ReceiptManagement.RESTfulAPI.Models;
 
 namespace ReceiptManagement.RESTfulAPI.Controllers
 {
     public class LoginController : ApiController
     {
-        public void Post(string username)
+        public HttpResponseMessage Post(Models.Login loginData)
         {
+            try
+            {
+                // Write login authentication code here
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
 
+            return Request.CreateResponse(HttpStatusCode.OK, new { Name = "Wahaj", Age="27" });
         }
 
-        public string Get(string username, string password)
-        {
-            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
-                return Boolean.TrueString.ToLower();
-            else
-                return Boolean.FalseString.ToLower();
-        }
 
-        // GET api/values/5
-        public string Get(string username)
-        {
-            return "this is your value="+username;
-        }
     }
 }
