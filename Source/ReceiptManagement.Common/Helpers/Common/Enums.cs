5909cc81d84f7b0a75e96e22021cc5457259a0fc
m-faisalnasir <mfaisal.nasir@hotmail.com>
2014-05-18 06:59:22 +0500

    added enum file

---
+++ Source/ReceiptManagement.Common/Helpers/Common/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptManagement.Common.Helpers.Common
+{
+    public class Roles
+    {
+        public const int User = 1;
+        public const int Admin = 2;
+    }
+}
