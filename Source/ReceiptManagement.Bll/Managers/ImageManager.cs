32aa9f3fcfe7d99f626e47b76ef5cbfc9470ca18
m-faisalnasir <mfaisal.nasir@hotmail.com>
2014-05-25 01:40:22 +0500

    added image manager

---
+++ Source/ReceiptManagement.Bll/Managers/ImageManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptManagement.Common.Helpers;
+using Entities = ReceiptManagement.Common.Entities;
+using CoreManagers = ReceiptManagement.Core.Managers;
+
+namespace ReceiptManagement.Bll.Managers
+{
+    public class ImageManager
+    {
+        #region List
+
+        public List<Entities.Image> Get(ApiContext apiContext,int userId)
+        {
+            var list = new List<Entities.Image>();
+
+            try
+            {
+                var querySettings = QuerySettings<Entities.Image>.Factory();
+
+                //Include Entities             
+
+                //Filter records
+                querySettings.WhereExpression = rc => rc.UserId == userId;
+
+                CoreManagers.ImageManager.Get(apiContext, querySettings, out list);               
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+        #region Insert
+
+        /// <summary>
+        ///     Insert
+        /// </summary>
+        /// <param name = "apiContext"></param>
+        /// <param name = "info"></param>
+        /// <returns></returns>
+        public bool Insert(ApiContext apiContext, Common.Entities.Image image)
+        {
+            try
+            {
+                var result = CoreManagers.ImageManager.Add(apiContext, new List<Common.Entities.Image> { image });
+
+                if (!result.WasSuccessful)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
