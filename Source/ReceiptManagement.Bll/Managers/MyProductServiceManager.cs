49d2d17ddc0b7df5febaf720a4f5689a1ccabfdd
m-faisalnasir <mfaisal.nasir@hotmail.com>
2014-05-25 19:19:09 +0500

    added managers for images tables

---
+++ Source/ReceiptManagement.Bll/Managers/MyProductServiceManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptManagement.Common.Helpers;
+using Entities = ReceiptManagement.Common.Entities;
+using CoreManagers = ReceiptManagement.Core.Managers;
+
+namespace ReceiptManagement.Bll.Managers
+{
+    public class MyProductServiceManager
+    {
+        #region Insert
+
+        /// <summary>
+        ///     Insert
+        /// </summary>
+        /// <param name = "apiContext"></param>
+        /// <param name = "info"></param>
+        /// <returns></returns>
+        public bool Insert(ApiContext apiContext, Common.Entities.My_Products_Services product)
+        {
+            try
+            {
+                var result = CoreManagers.MyProductsServicesManager.Add(apiContext, new List<Common.Entities.My_Products_Services> { product });
+
+                if (!result.WasSuccessful)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        ///     Insert
+        /// </summary>
+        /// <param name = "apiContext"></param>
+        /// <param name = "info"></param>
+        /// <returns></returns>
+        public bool Insert(ApiContext apiContext, Common.Entities.My_Products_Services product,out long id)
+        {
+            try
+            {
+                var result = CoreManagers.MyProductsServicesManager.Add(apiContext, product,out id);
+
+                if (!result.WasSuccessful)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Source/ReceiptManagement.Bll/Managers/WarrantyCardImageManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReceiptManagement.Common.Helpers;
+using Entities = ReceiptManagement.Common.Entities;
+using CoreManagers = ReceiptManagement.Core.Managers;
+
+namespace ReceiptManagement.Bll.Managers
+{
+    public class WarrantyCardImageManager
+    {
+        #region List
+
+        public List<Entities.WarrantyCardImage> Get(ApiContext apiContext,int userId)
+        {
+            var list = new List<Entities.WarrantyCardImage>();
+
+            try
+            {
+                var querySettings = QuerySettings<Entities.WarrantyCardImage>.Factory();
+
+                //Include Entities             
+
+                //Filter records
+                //querySettings.WhereExpression = rc => rc.UserId == userId;
+
+                CoreManagers.WarrantycardimageManager.Get(apiContext, querySettings, out list);               
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+
+        #region Insert
+
+        /// <summary>
+        ///     Insert
+        /// </summary>
+        /// <param name = "apiContext"></param>
+        /// <param name = "info"></param>
+        /// <returns></returns>
+        public bool Insert(ApiContext apiContext, Common.Entities.WarrantyCardImage warrantyCardImage,out long id)
+        {
+            try
+            {
+                var result = CoreManagers.WarrantycardimageManager.Add(apiContext, warrantyCardImage,out id);
+
+                if (!result.WasSuccessful)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
